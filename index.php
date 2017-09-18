4e1859c712215128fa922ad7ae00f0aef73d874d
Freddy Liu <freddy.liu@offis.com.au>
2017-09-18 14:53:32 +1000

    db password

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
                         <?php
                             $servername = "localhost";
                             $username = "root";
-                            $password = "";
+                            $password = "root";
                             $dbname = "demo_app";
                             $conn = new mysqli($servername, $username, $password, $dbname);
                             if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
