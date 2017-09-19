97cc3cf1cd53efe7dbccf249109427974744581d
Freddy Liu <freddy.liu@offis.com.au>
2017-09-19 11:49:29 +1000

    db

--- demo_app.sql
+++ demo_app.sql
@@ -29,7 +29,6 @@ USE `demo_app`;
 -- Table structure for table `city`
 --
 
-DROP TABLE IF EXISTS `city`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `city` (
